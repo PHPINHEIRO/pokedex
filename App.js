17704fdc25804aae386ab99b3993a20680e6089e
Pedro Henrique <phgp25@gmail.com>
2019-03-12 14:46:43 -0300

    refactoring

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
-import MyHeader from './src/components/Header';
+
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <MyHeader></MyHeader>
       </View>
     );
   }
